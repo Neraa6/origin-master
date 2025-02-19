8c2898be2d8e94ff67d9a7ee0390d3d900a5ff85
Neraa6 <yusufregan06@gmail.com>
2025-02-19 11:17:16 +0700

    add

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,20 @@
+import  { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AuthContext;
