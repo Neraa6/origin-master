e978a3b030c9511501768af97eb414f4c4c04b5b
Neraa6 <yusufregan06@gmail.com>
2025-02-22 22:09:07 +0700

    add

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,13 +1,14 @@
-import  { createContext, useState } from 'react';
-import PropTypes from 'prop-types';
+import { createContext, useState } from 'react';
+import PropTypes from 'prop-types'; 
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // Tambahkan state ini
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, isAuthenticated, setIsAuthenticated }}>
       {children}
     </AuthContext.Provider>
   );
