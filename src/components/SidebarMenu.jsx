17a544ef2221d11c0da279c41f06ffad38a1314d
Neraa6 <yusufregan06@gmail.com>
2025-02-27 21:44:01 +0700

    add

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,7 +1,37 @@
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+
+import { useContext } from "react";
+
+
+import Cookies from 'js-cookie'
+
+
+import  AuthContext  from '../context/AuthContext';
 
 export default function SidebarMenu() {
+
+  
+    const navigate = useNavigate();
+
+  
+    const { setIsAuthenticated } = useContext(AuthContext);
+
+   
+    const logout = () => {
+
+       
+        Cookies.remove('token');
+        Cookies.remove('user');
+
+     
+        setIsAuthenticated(false);
+
+   
+        navigate("/login", { replace: true });
+    }
+
     return (
         <div className="card border-0 rounded shadow-sm">
             <div className="card-header">
@@ -12,7 +42,7 @@ export default function SidebarMenu() {
                     <Link to="/admin/dashboard" className="list-group-item list-group-item-action">Dashboard</Link>
 
                     <Link to="/admin/users" className="list-group-item list-group-item-action">Users</Link>
-                    <a href="#" className="list-group-item list-group-item-action" style={{ cursor: 'pointer' }}>Logout</a>
+                    <a onClick={logout} className="list-group-item list-group-item-action" style={{ cursor: 'pointer' }}>Logout</a>
                 </div>
             </div>
         </div>
